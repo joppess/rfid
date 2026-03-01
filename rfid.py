ea8af4ef016816587d309b7b5b2420c5f0413cdd
joppess <johanlidback@hotmail.com>
2026-03-01 23:46:48 +0100

    start

--- rfid.py
+++ rfid.py
@@ -1,1 +1,15 @@
 # just testing
+from smartcard.System import readers
+from smartcard.util import toHexString
+import time
+
+def start_system():
+    # kollar om vi hittar inkopplat läsare
+    found_reader = readers()
+    if not found_reader:
+        print("Did not find a reader")
+        return
+    
+    my_reader = found_reader[0]
+    print(f"System is ready, connected to: {my_reader}")
+    print("/nWaiting for card...(press ctrl + c to shutdown)")
